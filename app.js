1bac7a22dd38e628674eca14db46581f4974de26
Lin <mt5718214@gmail.com>
2020-08-28 15:16:29 +0800

    set static files

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//set static files
+app.use(express.static('public'))
+
 //route setting
 app.get('/', (req, res) => {
   res.render('index')
