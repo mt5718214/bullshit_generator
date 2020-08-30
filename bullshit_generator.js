31cfb6b487f07de0699cb1d1b8d760e6146fd986
Lin <mt5718214@gmail.com>
2020-08-30 17:59:48 +0800

    modify if condition to process error

--- bullshit_generator.js
+++ bullshit_generator.js
@@ -5,9 +5,19 @@ function bullshit_generator(option) {
     entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
   }
   const phrase = ['很簡單', '很容易', '很快', '很正常']
+  const jobs = {
+    engineer: '工程師',
+    entrepreneur: '創業家',
+    designer: '設計師'
+  }
 
   const target = option.target
   let bullshit = ''
+
+  //處理錯誤
+  if (!target) {
+    return '請選擇一個對象'
+  }
 
   //選擇該類別的隨機task
   const randomTask = randomDryTalk(task[target])
@@ -16,7 +26,7 @@ function bullshit_generator(option) {
   const randomPhrase = randomDryTalk(phrase)
 
   //回傳結果
-  return bullshit += `身為一個${target} ${randomTask} ${randomPhrase}吧!!`
+  return bullshit += `身為一個${jobs[target]} ${randomTask} ${randomPhrase}吧!!`
 }
 
 function randomDryTalk(array) {
