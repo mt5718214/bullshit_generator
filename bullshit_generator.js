d23edfc671291d8d1201d6eec9fed51b853e0f65
Lin <mt5718214@gmail.com>
2020-08-30 17:40:16 +0800

    add  bullshit_generator

---
+++ bullshit_generator.js
@@ -0,0 +1,28 @@
+function bullshit_generator(option) {
+  const task = {
+    engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
+    designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
+    entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
+  }
+  const phrase = ['很簡單', '很容易', '很快', '很正常']
+
+  const target = option.target
+  let bullshit = ''
+
+  //選擇該類別的隨機task
+  const randomTask = randomDryTalk(task[target])
+
+  //選擇隨機的phrase
+  const randomPhrase = randomDryTalk(phrase)
+
+  //回傳結果
+  return bullshit += `身為一個${target} ${randomTask} ${randomPhrase}吧!!`
+}
+
+function randomDryTalk(array) {
+  const randomIndex = Math.floor(Math.random() * array.length)
+  return array[randomIndex]
+}
+
+//export function for other files to use
+module.exports = bullshit_generator
