0a8fee5476aaa5d09fee8a69abed53fa527472d1
Lin <mt5718214@gmail.com>
2020-08-30 17:38:53 +0800

    add body-parser

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const bullshit_generator = require('./bullshit_generator')
 
 //set template-engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+// setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //set static files
 app.use(express.static('public'))
@@ -13,6 +18,11 @@ app.use(express.static('public'))
 //route setting
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/generate', (req, res) => {
+  const bullshit = bullshit_generator(req.body)
+  res.render('index', { bullshit: bullshit })
 })
 
 
